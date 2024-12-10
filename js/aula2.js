cdc32f17dce10303a0802d74aeed62a78a29a485
PauloRoberto82 <pr.tech82@hotmail.com>
2024-12-10 20:56:54 -0300

    Primeiras aulas
    Aulas 1 e 2

---
+++ js/aula2.js
@@ -0,0 +1,3 @@
+let name = "Paulo"
+
+console.log(`Meu nome ${name}. ` + "Estou aprendendo JavaScript às " + 20 +  "horas" )
---
+++ js/aula5.js
@@ -0,0 +1,3 @@
+console.log("Hello World" ) //String
+console.log(123) //Number
+console.log(12.34) //Number (flutuante)
