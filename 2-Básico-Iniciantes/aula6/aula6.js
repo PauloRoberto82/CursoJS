b88982f7328f709e6ee4e3af906a0d586892ceb8
PauloRoberto82 <pr.tech82@hotmail.com>
2025-01-02 21:03:04 -0300

    Conceitos const, let e typeof

--- 2-Básico-Iniciantes/aula6/aula6.js
+++ 2-Básico-Iniciantes/aula6/aula6.js
@@ -1,6 +1,11 @@
  let nomeCliente = "PRTech";
     console.log(nomeCliente);
 
+let nome = "PRTech";
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+console.log("O meu nome é " + nome);
+
  let nomeCompletoCliente = "PRTech Treinamentos";
     console.log(nomeCompletoCliente);
 
--- 2-Básico-Iniciantes/aula7/aula7.js
+++ 2-Básico-Iniciantes/aula7/aula7.js
@@ -1,1 +1,32 @@
- 
+ //Não podemos criar constantes com palavras reservadas
+ //Contantes precisam ter nomes significativos
+ //NÃO UTILIZE VAR, UTILIZE CONST.
+//CONSTANTE TEM QUE TER UM VALOR INICIAL/ATRIBUIÇÃO
+
+const nome = 'João';
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const resultado = primeiroNumero + segundoNumero;
+const resultado1 = primeiroNumero - segundoNumero;
+const resultado2 = primeiroNumero * segundoNumero;
+const resultado3 = primeiroNumero / segundoNumero;
+
+console.log(resultado);
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+
+let resultadoDuplicado = resultado * 3;
+resultadoDuplicado = resultadoDuplicado + 10;
+console.log(resultadoDuplicado);
+
+console.log(typeof resultado)
+
+const numeroConcatenado = "5"
+console.log(numeroConcatenado + 50);
+//Quando concatenamos strings com números, o resultado do typeof é uma string.
+console.log(typeof(numeroConcatenado + 50))
+
+
